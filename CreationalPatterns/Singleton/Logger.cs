6bb33522d312aad0bfe45fed7d0a0b6118f71b2d
Saikat30101997 <saikat30101997@gmail.com>
2021-10-14 11:02:49 +0600

    add singletondesignpattern

---
+++ CreationalPatterns/Singleton/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Logger
+    {
+        private static Logger _logger;
+
+        public string TestValue { get; set; }
+        private Logger() // ei private constructor create korar karon e baire thke kew r instace create korte parbe na
+        {
+            TestValue = DateTime.Now.ToString();
+        }
+
+        //ensure korbo j eta ektar besi instance create korbo na
+        public static Logger CreateLogger()
+        {
+            if (_logger == null)
+                _logger = new Logger();   // ei duita line multithread create korte hobe naile inconsistency create krbe
+
+            return _logger;
+        
+        }
+
+
+      
+    }
+}
---
+++ CreationalPatterns/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger l1 = Logger.CreateLogger();
+            Logger l2 = Logger.CreateLogger();
+            Console.WriteLine(l1.TestValue);
+            Console.WriteLine(l2.TestValue);
+
+            if(l1==l2)
+                Console.WriteLine("Same object");
+        }
+    }
+}
