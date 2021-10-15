b75c7c01a967afa0e8d96b797213129a0d3b5f0c
Saikat30101997 <saikat30101997@gmail.com>
2021-10-15 17:14:00 +0600

    added factorydesignpattern

---
+++ CreationalPatterns/Factory/ProductFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class ProductFactory
+    {
+        public static IProduct CreateProduct<T>(string name, double price, double? discount)
+            where T : IProduct, new()
+        {
+            IProduct product;
+            product = new T();
+
+            product.Name = name;
+
+            if (discount.HasValue)
+                product.Price = price * discount.Value / 100.0;
+            else
+                product.Price = price;
+
+            return product;
+        }
+    }
+}
---
+++ CreationalPatterns/Factory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Camera";
+            var price = 2000;
+            double? discount = 20;
+
+            IProduct product = ProductFactory.CreateProduct<Stationary>(name, price, discount);
+
+            Console.WriteLine(product.Price);
+            Console.WriteLine(product.GetType());
+        }
+    }
+}
